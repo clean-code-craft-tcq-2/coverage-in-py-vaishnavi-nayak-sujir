e4b5d6bdbf25161a6ae967d3cc7edaf7906cf6d2
vaishnavi-nayak-sujir <vaishnavinayak86@gmail.com>
2022-02-23 17:20:51 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -26,11 +26,10 @@ def check_and_alert(alertTarget, batteryChar, temperatureInC):
   breachType =\
     classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
   if alertTarget == 'TO_CONTROLLER':
-    send_to_controller(breachType)
-    return 1
+    status = send_to_controller(breachType)
   elif alertTarget == 'TO_EMAIL':
-    send_to_email(breachType)
-    return 1
+    status = send_to_email(breachType)
+  return status
 
 
 def send_to_controller(breachType):
