cc8efb17bf3711349b16443b0208b4a63b31fc68
vaishnavi-nayak-sujir <vaishnavinayak86@gmail.com>
2022-02-23 17:08:30 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -9,6 +9,7 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.infer_breach(110, 50, 100) == 'TOO_HIGH')
     self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING', 100) == 'TOO_HIGH')
     self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER',{'coolingType': 'PASSIVE_COOLING'}, 100) == 1)
+    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL',{'coolingType': 'PASSIVE_COOLING'}, 100) == 1)
 
 
 if __name__ == '__main__':
